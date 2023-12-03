fea123981ed164d8e4a6ef3ebaac70b983678bc4
Morteza <maleki.morteza92@gmail.com>
2023-12-03 20:01:19 +0330

    added size limit for imgs

--- pdf_images.py
+++ pdf_images.py
@@ -1,7 +1,7 @@
 import fitz  # PyMuPDF
 import os
 
-def extract_images_from_pdf(pdf_path, output_folder):
+def extract_images_from_pdf(pdf_path, output_folder, size_limit):
     # Open the PDF file
     doc = fitz.open(pdf_path)
 
@@ -16,18 +16,20 @@ def extract_images_from_pdf(pdf_path, output_folder):
             base_image = doc.extract_image(xref)
             image_bytes = base_image["image"]
 
-            # Define the image's output path
-            image_name = f"{os.path.splitext(os.path.basename(pdf_path))[0]}_page_{i}_img_{image_index}.png"
-            image_output_path = os.path.join(output_folder, image_name)
+            # Check the size of the image
+            if len(image_bytes) <= size_limit:
+                # Define the image's output path
+                image_name = f"{os.path.splitext(os.path.basename(pdf_path))[0]}_page_{i}_img_{image_index}.png"
+                image_output_path = os.path.join(output_folder, image_name)
 
-            # Save the image
-            with open(image_output_path, "wb") as img_file:
-                img_file.write(image_bytes)
+                # Save the image
+                with open(image_output_path, "wb") as img_file:
+                    img_file.write(image_bytes)
 
     # Close the document
     doc.close()
 
-def extract_images_from_directory(directory_path, output_folder):
+def extract_images_from_directory(directory_path, output_folder, size_limit):
     # Create the output directory if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -37,9 +39,12 @@ def extract_images_from_directory(directory_path, output_folder):
         for file in files:
             if file.lower().endswith('.pdf'):
                 pdf_path = os.path.join(root, file)
-                extract_images_from_pdf(pdf_path, output_folder)
+                extract_images_from_pdf(pdf_path, output_folder, size_limit)
 
 # Usage
-directory_path = 'path_to_your_directory'
+directory_path = '/home/drghodrat/Zotero/storage'
 output_folder = 'imgs'
-extract_images_from_directory(directory_path, output_folder)
+if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
+size_limit = 100000  # 100 KB as an example
+extract_images_from_directory(directory_path, output_folder, size_limit)
